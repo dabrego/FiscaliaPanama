5b7b61b19437e500eb26a25088a29e7e085014f5
Domingo Abrego <dabrego@gmail.com>
2017-07-20 23:13:37 -0500

    controller changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $newUser = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        $newUser->attachRole($adminRole);
+        return $newUser;
+
     }
 }
