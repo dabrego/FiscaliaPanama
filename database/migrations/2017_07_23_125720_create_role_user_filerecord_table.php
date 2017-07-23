6f012b73712a59e7090fc8e039c7ddf6497949e3
Domingo Abrego <dabrego@gmail.com>
2017-07-23 15:42:10 -0500

    Creando dropdown de nombres de jueces para creación de expedientes  y modelo nuevo para accesar pivot table Se borro, carpeta de vistas aview

---
+++ database/migrations/2017_07_23_125720_create_role_user_filerecord_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleUserFilerecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pivot', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->timestamps();
+
+            
+            $table->integer('role_id')->unsigned()->index();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->integer('filerecord_id')->unsigned()->index();
+            $table->foreign('filerecord_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pivot');
+    }
+}
